302d82ec1676de0f3c55c0efa04d8709406acf5f
PremCoder <kasturawwashunnamani@gmail.com>
2026-05-08 17:31:17 +0530

    chapter 5 started

--- app/src/main/java/com/example/a5thchapter/MainActivity.java
+++ app/src/main/java/com/example/a5thchapter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a5thchapter;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +11,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        listView = findViewById(R.id.ListView);
     }
 }
